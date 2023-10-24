b8f68b6d9152026a5f489355ce09f22bcdb136db
osmanjosue <osmanjosue@gmail.com>
2023-10-24 15:01:38 -0600

    Seccion 4 => Titulo: comunicacion entre componentes-Props

--- src/FirsApp.jsx
+++ src/FirsApp.jsx
@@ -1,15 +1,20 @@
 //RAFTC PARA SNIPET
 
-export const FirsApp = () => {
+export const FirsApp = ( { title, subTitle } ) => {
+
+    /* console.log(props); */
+
 return (
         <>
-            <h1>Fernando</h1>
+            <h1>{title}</h1>
             {/* <code>{JSON.stringify(newMessage)}</code> */}
-            <p>Soy un subtitulo</p>
+            <p>{ subTitle + 1 }</p>
         </>
 /*  <Fragment>
             <h1>Osman Herrera</h1>
             <p>Spy un subtitulo</p>
         </Fragment> */
 )
+
+
 }
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,6 @@ import './styles.css';
 
     ReactDOM.createRoot(document.getElementById('root')).render(
         <React.StrictMode>
-            <FirsApp></FirsApp>
+            <FirsApp title = "Hola soy goku" subTitle={123} />  {/* subTitle = "123" asigna un string pero subTitle = { 123 } asigna numero */}
         </React.StrictMode>
     );
