cb90b7d9faca341efd2d069e34e74abb842b4469
osmanjosue <osmanjosue@gmail.com>
2023-10-25 10:59:37 -0600

    Seccion 4 => Titulo: HandleSubstract y handleReset

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -10,14 +10,23 @@ export const CounterApp = ({value}) => {
         setCounter(counter + 1);
         /* setCounter((c) => c+1) */ // hace lo mismo que la linea de arriba
     }
+    
+    const handleSubstract = () => {
+        setCounter(counter - 1);        
+    }
+
+    const handleReset = () => {
+        setCounter(value);
+    }
 
 return (
         <>
             <h1>CounterApp</h1>
             <h2> { counter } </h2>
-            <button onClick={ handleAdd }>
-                +1
-            </button>
+            
+            <button onClick={ handleAdd }> +1 </button>
+            <button onClick= { handleSubstract }> -1 </button>
+            <button onClick= { handleReset }> Reset </button>
         </>
         );
 }
