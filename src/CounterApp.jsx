0f461bba0b7ec5103e513eab9409b70ab6e15782
osmanjosue <osmanjosue@gmail.com>
2023-10-25 10:03:46 -0600

    Seccion 4 => Titulo: Evento click (Eventos en general)

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,16 +1,25 @@
 import PropTypes from 'prop-types';
 
 export const CounterApp = ({value}) => {
+
+    const handleAdd = () => {
+        console.log('+1')
+        value = 1000;
+    }
+
 return (
         <>
             <h1>CounterApp</h1>
-            <h2>{value}</h2>
+            <h2> { value } </h2>
+            <button onClick={ handleAdd }>
+                +1
+            </button>
         </>
-        )
+        );
 }
 
 CounterApp.propTypes = {
-    value: PropTypes.number,
+    value: PropTypes.number.isRequired,
 }
 
 
