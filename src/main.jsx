47799254169924a81389a16d5b9e206903e28074
osmanjosue <osmanjosue@gmail.com>
2023-10-25 09:14:52 -0600

    Tarea hecha hasta este punto por mi - Componente CounterApp

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom/client';
 import {FirstApp} from './FirstApp';
 /* import {HelloWorldApp} from './HelloWorldApp' */
 import './styles.css';
+import {CounterApp} from './CounterApp';
 
 
     ReactDOM.createRoot(document.getElementById('root')).render(
         <React.StrictMode>
-            <FirstApp/>
+            <CounterApp value = {123}/>
+            {/* <FirstApp/> */}
         </React.StrictMode>
     );
