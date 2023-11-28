c7805da6970d16660151bc051f3bf6f2fcf3f4a9
osmanjosue <osmanjosue@gmail.com>
2023-11-27 21:08:41 -0600

    Ejercicio de pruebas con async-await

---
+++ test/base-pruebas/11-async-await.test.js
@@ -0,0 +1,13 @@
+import { getImagen } from "../../src/basepruebas/base-pruebas/11-async-await"
+
+describe('Pruebas en 11-async-await', () => {
+    test('getImagen debe de retornar un URL de la imagen', async() => {
+
+        const url = await getImagen();
+        console.log(url);
+        expect( typeof url ). toBe( 'string' );
+      
+    })
+    
+  
+})
