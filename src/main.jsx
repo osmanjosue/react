f0e6a427514f5475a7ebfac1ac5ad2500aad58e1
osmanjosue <osmanjosue@gmail.com>
2023-12-04 11:29:32 -0600

    Ejercicio Evaluar el Catch en el async-await

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {CounterApp} from './CounterApp';
 
 import {FirstApp} from './FirstApp';
 /* import {HelloWorldApp} from './HelloWorldApp' */
 import './styles.css';
-import {CounterApp} from './CounterApp';
 
 
     ReactDOM.createRoot(document.getElementById('root')).render(
         <React.StrictMode>
-            <CounterApp value = { 20 }/>
-            {/* <FirstApp/> */}
+            {/* <CounterApp value = { 20 }/> */}
+            <FirstApp />
         </React.StrictMode>
     );
--- test/base-pruebas/11-async-await.test.js
+++ test/base-pruebas/11-async-await.test.js
@@ -1,11 +1,10 @@
 import { getImagen } from "../../src/basepruebas/base-pruebas/11-async-await"
 
 describe('Pruebas en 11-async-await', () => {
-    test('getImagen debe de retornar un URL de la imagen', async() => {
+    test('getImagen debe de retornar un error si no tenemos api key', async() => {
 
-        const url = await getImagen();
-        console.log(url);
-        expect( typeof url ). toBe( 'string' );
+        const resp = await getImagen();
+        expect( resp ). toBe( 'No se encontro la imagen' );
       
     })
     
